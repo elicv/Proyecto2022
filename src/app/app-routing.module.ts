5a2c501e33adf496c246a0208478ddb2b1898a45
elicv <elison1606@gmail.com>
2022-04-20 01:34:29 -0600

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
-
-import { CrearcuentaComponent } from './Components/crearcuenta/crearcuenta.component';
+import { CrearcuentaComponent } from'./Components/crearcuenta/crearcuenta.component';
+import {AcercadeComponent} from './Components/acercade/acercade.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
   {path: 'crearcuenta', component: CrearcuentaComponent},
+  {path: 'acercade', component: AcercadeComponent},
   {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ];
 
