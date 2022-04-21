b1b4cfb7d5197fffaa8f8ff7a84880a8f3693f82
elicv <elison1606@gmail.com>
2022-04-20 21:44:05 -0600

    1 file modified, 1 deleted

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/Services/login.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-home',
@@ -9,11 +11,15 @@ import { LoginService } from 'src/app/Services/login.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private loginService: LoginService) { }
+  constructor(private router: Router, private loginService: LoginService,private firestore: AngularFirestore) { }
 
   volver(){
     this.loginService.logout();
     this.router.navigate(['/login']);
+  }
+
+  altausuario(){
+    this.firestore.cole
   }
 
   ngOnInit(): void {
