1f2b8fb2b2501d68eb74843fce09b0b9ade42492
elicv <elison1606@gmail.com>
2022-04-23 13:55:55 -0600

    2 files modified, 1 added, 1 deleted

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/Services/login.service';
 
+import { Producto } from 'src/productos.model';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,17 @@ import { LoginService } from 'src/app/Services/login.service';
 })
 export class HomeComponent implements OnInit {
 
-  data = {
-    id: 'a',
-    comida: '1'
+  producto: Producto = {
+    id: '',
+    nombre: '',
+    precio: '',
+    descripcion: ''
   }
+  
 
-  listaUsuarios: Array<unknown> = [];
+  listaproductos: Array<unknown> = [];
 
-  productos: productos[] = [];
+  productos: Producto[] = [];
   
 
   constructor(private router: Router, private loginService: LoginService) {}
@@ -27,10 +31,7 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/login']);
   }
 
-  altausuario() {
-    this.loginService.createcollection('Ciudad', this.data);
-    
-  }
+
 
   ngOnInit(): void {
 
